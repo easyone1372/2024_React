e34168edaa95dd8349639d62819710420153959a
easyone1372 <74178762+easyone1372@users.noreply.github.com>
2024-06-14 11:32:40 +0900

    2024.06.14

---
+++ day03/App.tsx
@@ -0,0 +1,12 @@
+// import React, { CSSProperties } from "react";
+import HeaderQuiz from "./HeaderQuiz";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderQuiz />
+    </div>
+  );
+}
+
+export default App;
