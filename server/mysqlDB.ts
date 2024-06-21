07ad8306c7c1eceeff5b68cad22400adfb74c965
easyone1372 <74178762+easyone1372@users.noreply.github.com>
2024-06-21 12:40:23 +0900

    2024.06.21 server 관련

---
+++ server/mysqlDB.ts
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const mysql = require("mysql2");
+// const app = express();
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "6428",
+//   database: "reactspring",
+// });
+
+// connection.connect();
+// connection.query(
+//   `insert into coffee (id,name,price,calories) values (6,"밀크티",4500,300)`
+// );
+// connection.end();
